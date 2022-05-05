29852749e04a20d911dd178bcc75434625e468a2
Lord Dickenstein <jddestacamento@iskolarngbayan.pup.edu.ph>
2022-05-06 06:25:31 +0800

    Added AUTH_USER_MODEL for custom made User Model

--- pupweb/pupweb/settings.py
+++ pupweb/pupweb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,13 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.UserProfile'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
